528a18fdad58aa1af59c22362589a5bbe0d2bdeb
Irenegarver <igarver3107@m.educaand.es>
2023-01-20 10:26:08 +0100

    day 2

---
+++ day 2/variables.py
@@ -0,0 +1,63 @@
+#'Day 2: 30 Days of python programming'
+first_name = 'Jose Luis'
+last_name = 'Jiménez'
+full_name = 'José Luis Jiménez Bandera'
+country = 'España'
+city = 'Jerez de la Frontera'
+age = '17'
+year = '2022'
+is_married ='no'
+is_true = 'yes'
+is_light_on = 'no'
+country,city = 'España','Jerez de la Frontera'
+
+#level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(country,city))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one=5
+num_two=4
+
+num_total=num_one+num_two
+print(num_total)
+
+num_diff=num_one-num_two
+print(num_diff)
+
+num_product=num_one * num_two
+print(num_product)
+
+num_divide=num_one/num_two
+print(num_divide)
+
+num_exp=num_two**num_one
+print(num_exp)
+
+floor_divide=num_one//num_two
+print(floor_divide)
+
+radius=30
+area_of_circle=3.14 * radius**2
+print(area_of_circle)
+circumference_of_circle=2 * 3.14 * radius
+print(circumference_of_circle)
+
+r=input ("¿cual es el radio)"
+r2= int(r)
+area=3.14 * r2 **2
+print(area)
+
+Run 
+help('keywords')
