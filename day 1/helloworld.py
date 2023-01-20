39710b66d682dfe2d88d4d4d8edd78366558323c
Irenegarver <igarver3107@m.educaand.es>
2023-01-20 10:24:29 +0100

    day 1

---
+++ day 1/helloworld.py
@@ -0,0 +1,23 @@
+print(type('your name: Jose Luis'))
+print(type('your family name: Jiménez'))
+print(type('your country:España'))
+print(type('I am enjoying 30 days of python'))
+print(type('10'))
+print(type('9.8'))
+print(type('3.14'))
+print(type('4.4'))
+print['Asabeneh', 'Python', 'Finland']
+print(type('your name: Jose Luis'))
+print(type('your family name: Jiménez'))
+print(type('your country:España'))
+
+print(type(10))
+print(type(3,14))
+print(type(1+3j))
+print(type('Asabeneh'))
+print(type([1,2,3]))
+print(type({'name':'Asabeneh'}))
+print(type({9.8, 3.14, 2.7}))
+print(2 - 10)
+print(3 - 8)
+
