8c5f15176e6037fd8bf687125ed7547bf3cfec28
joselujimeenee <joseluisjimenezbandera@gmail.com>
2023-03-10 10:31:52 +0100

    day 6 y 8

---
+++ day 6/tuples.py
@@ -0,0 +1,16 @@
+empty_tuple=()
+
+tpl=('Brothers')
+Brothers=('Marcos' , 'Paco' , 'Luis' , 'Juan')
+print(Brothers)
+
+tpl=('Sisters')
+Sisters=('Lucia' , 'Helena' , 'Susana' , 'Carmen')
+print=(Sisters)
+
+tuple=('Family')
+Family=('Brothers'+'Sisters')
+print=(Family)
+
+fatherandmother=Family
+print(fatherandmother)
