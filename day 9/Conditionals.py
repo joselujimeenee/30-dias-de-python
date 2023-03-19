edbd453a03056cc46e791688560658011ea4b103
joselujimeenee <joseluisjimenezbandera@gmail.com>
2023-03-19 12:56:31 +0100

    dia 9

---
+++ day 9/Conditionals.py
@@ -0,0 +1,59 @@
+age= 17
+if age > 18:
+    print('you are old enougth to drive')
+if age < 18:
+    print('you need one more year to learn to drive')
+
+my_age=17
+your_age=18
+if my_age > 18:
+    print('soy mayor que tu') if my_age < 18 else print('tu eres mayor que yo')
+
+a=9
+b=5
+if a > b:
+    print('nueve es mayor que cinco')
+elif a < b:
+    print('nueve es menor que cinco')
+else:
+    print('a es igual a b')
+
+A=80-100
+B=70-79
+C=60-69
+D=50-59
+F= 0-49
+if A > 80:
+    print('sobresaliente')
+if B > 70:
+    print('notable')
+if C > 60:
+    print('bien')
+
+if D > 50: 
+    print('suficiente')
+
+if F > 0:
+    print('suspenso')
+
+otoño=['septiembre' , 'octubre' , 'noviembre']
+invierno=['diciembre' , 'enero' , 'febrero']
+primavera=['marzo' , 'abril' , 'mayo']
+verano=['junio' , 'julio' , 'agosto']
+
+if 'septiembre':
+    print('es otoño')
+if 'enero':
+    print('es invierno')
+if 'mayo':
+    print('es primavera')
+if 'junio':
+    print('es verano')
+
+fruits=['banana', 'orange', 'mango', 'lemon']
+if 'lemon' is False:
+    fruits.append('coconut')
+    print('coconut')
+if 'lemon' is True:
+    print('esa fruta ya está')
+
